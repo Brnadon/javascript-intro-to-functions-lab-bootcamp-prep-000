640dba24d4f60b1d02eeef5f6d68173daaf94136
Brandon Brasson <rocksong622@gmail.com>
2017-08-31 00:26:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,15 @@ function shout(string) {
 function shout(string) {
   return string.toUpperCase()
 }
+
+function sayHiToGrandma(string) {
+}
+
+functions sayHiToGrandma(string) {
+   return string.toLowerCase() === lowercase // true
+
+   'HELLO'.toLowerCase()
+
+   function  sayHiToGrandma(string) {
+  return string
+}
